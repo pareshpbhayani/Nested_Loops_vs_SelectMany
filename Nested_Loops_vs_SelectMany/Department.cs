ffba3a52b72da4e34d9946a852d4aadc26b7b3f9
pareshpbhayani <pareshpbhayani@gmail.com>
2024-09-11 15:38:40 +0530

    Add project files.

---
+++ Nested_Loops_vs_SelectMany/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Loops_vs_SelectMany
+{
+    public class Department
+    {
+        public List<string> Employees;
+    }
+}
---
+++ Nested_Loops_vs_SelectMany/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Nested_Loops_vs_SelectMany;
+
+var departments = new List<Department>
+{
+    new Department { Employees = new List<string> { "Alice", "Bob" } },
+    new Department { Employees = new List<string> { "Charlie", "David" } }
+};
+
+Console.WriteLine("Using Nested Loops");
+
+var allEmployees = new List<string>();
+
+foreach (var department in departments)
+{
+    foreach (var employee in department.Employees)
+    {
+        allEmployees.Add(employee);
+    }
+}
+
+foreach (var employee in allEmployees)
+{
+    Console.WriteLine(employee);
+}
+
+Console.WriteLine();
+Console.WriteLine("Using SelectMany");
+
+// Using SelectMany to flatten the list
+var allEmployees2 = departments.SelectMany(d => d.Employees).ToList();
+
+foreach (var employee in allEmployees2)
+{
+    Console.WriteLine(employee);
+}
